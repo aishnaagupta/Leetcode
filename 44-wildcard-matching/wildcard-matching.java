c28788064fb7962a843e302c1c100a947ce2fde4
Aishnaa Gupta <aishnaa0011@gmail.com>
2026-09-26 01:33:16 +0530

    Time: 55 ms (20.10%) | Memory: 47.9 MB (33.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,46 +1,36 @@
-//MEMOIZATION
+//TABULATION
 class Solution {
     public boolean isMatch(String s, String p) {
         int n = p.length();
         int m = s.length();
-        Boolean[][] dp = new Boolean[n][m]; //B captical Boolean(T/F/NULL) because this is a wrapper class and can contain null values as inital; not boolean(T/F) becoz then inital values would be false;
+        boolean[][] dp = new boolean[n + 1][m + 1];
 
-        return wild(n - 1, m - 1, s, p, dp);
-    }
-
-    boolean wild(int i, int j, String s, String p, Boolean[][] dp) {
-
-        // Both strings completely matched
-        if (i < 0 && j < 0) {
-            return true;
+        //base cases after shifting of index:
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = false;
         }
-
-        // Pattern finished but string still has characters
-        if (i < 0 && j >= 0) {
-            return false;
-        }
-
-        // String finished, check whether remaining pattern
-        // consists entirely of '*'
-        if (j < 0 && i >= 0) {
-            for (int k = 0; k <= i; k++) {
-                if (p.charAt(k) != '*') {
-                    return false;
+        for (int i = 1; i <= n; i++) {
+            boolean flag = true;
+            for (int k = 1; k <= i; k++) {
+                if (p.charAt(k - 1) != '*') {
+                    flag = false;
+                    break;
                 }
             }
-            return true;
+            dp[i][0] = flag;
         }
 
-        if (dp[i][j] != null) {
-            return dp[i][j];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p.charAt(i - 1) == s.charAt(j - 1) || p.charAt(i - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(i - 1) == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                } else
+                    dp[i][j] = false;
+            }
         }
-
-        if (p.charAt(i) == s.charAt(j) || p.charAt(i) == '?') {
-            return dp[i][j] = wild(i - 1, j - 1, s, p, dp);
-        }
-        if (p.charAt(i) == '*') {
-            return dp[i][j] = wild(i - 1, j, s, p, dp) || wild(i, j - 1, s, p, dp);
-        }
-        return dp[i][j] = false;
+        return dp[n][m];
     }
 }
